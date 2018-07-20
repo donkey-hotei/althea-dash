9adbe8d6dbb1b61f13699dafc26fa9831a833fa5
donkey_hotei <donkey-hotei@cryptolab.net>
2018-07-20 16:45:58 -0700

    move normalizeNeighbors function to Neighbors component file

--- src/components/Neighbors/Neighbors.js
+++ src/components/Neighbors/Neighbors.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
 import { actions, connect } from '../../store';
 import NodeInfo from './NodeInfo';
-import { normalizeNeighbors } from '../../lib/utils';
+import {
+  getMinimumAtKey,
+  getMaximumAtKey,
+  logNormalize,
+} from '../../lib/utils';
 import './Neighbors.css';
+
+/**
+ * Log-normalizes current debt, total route metric to exit node, and link cost
+ * neigbors in neighbor data.
+ */
+const normalizeNeighbors = (neighborData) => {
+  const minCurrentDebt = getMinimumAtKey('currentDebt', neighborData);
+  const maxCurrentDebt = getMaximumAtKey('currentDebt', neighborData);
+  const minRouteMetricToExit = getMinimumAtKey('routeMetricToExit', neighborData);
+  const maxRouteMetricToExit = getMaximumAtKey('routeMetricToExit', neighborData);
+  const minLinkCost = getMinimumAtKey('linkCost', neighborData);
+  const maxLinkCost = getMaximumAtKey('linkCost', neighborData);
+
+  return neighborData.map(neighbor => ({
+    ...neighbor,
+    normalizedCurrentDebt: logNormalize(
+      Math.abs(neighbor.currentDebt),
+      minCurrentDebt,
+      maxCurrentDebt,
+    ),
+    normalizedRouteMetricToExit: logNormalize(
+      neighbor.routeMetricToExit,
+      minRouteMetricToExit,
+      maxRouteMetricToExit,
+    ),
+    normalizedLinkCost: logNormalize(
+      neighbor.linkCost,
+      minLinkCost,
+      maxLinkCost,
+    ),
+  }));
+};
 
 class Neighbors extends Component {
   componentDidMount() {
